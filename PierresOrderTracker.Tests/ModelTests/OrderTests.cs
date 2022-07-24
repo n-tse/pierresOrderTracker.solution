eebd97f61f38193b6584aa6e5d796aa970c2c8d8
Nick Tse <nicholashtse@gmail.com>
2022-07-24 07:44:20 -0700

    test orderConstructor; test passes

---
+++ PierresOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresOrderTracker.Models;
+using System;
+
+namespace PierresOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("descrition", "title", 1, "date");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    
+  }
+}
